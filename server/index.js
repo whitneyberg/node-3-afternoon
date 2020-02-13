8609a63c7bdbaded635ffa5f2e5457c50df15188
user <user@users-MacBook-Air.local>
2020-02-12 20:56:49 -0700

    whitnode3afternoon

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const massive = require('massive');
-const products_Controller = require('./products_Controller')
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const products_controller = require("./products_controller");
 
 const app = express();
 
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
-massive(CONNECTION_STRING).then(dbObj => {
-    app.set('db', dbObj);
-}).catch(err => console.log(err));
+massive(CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/api/products', products_Controller.create);
-app.get('/api/products', products_Controller.getAll);
-app.get('/api/products', products_Controller.getOne);
-app.put('/api/products', products_Controller.update);
-app.delete('/api/products', products_Controller.delete)
+app.post('/api/products', products_controller.create);
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.delete('/api/products/:id', products_controller.delete);
 
-app.listen(SERVER_PORT ,() => {console.log(`server listening on ${SERVER_PORT}`)});
-
-
-
-
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
